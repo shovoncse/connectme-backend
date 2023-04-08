2160889bb3b8660e3b52359036a56f115f315fb7
shovoncse <shovondas26@gmail.com>
2023-04-08 20:45:34 +0600

    route update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,16 +3,12 @@ const router = express.Router();
 const {
   loginUser,
   registerUser,
-  getUsersList,
   refreshToken,
   logOutUser
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
-router
-  .route('/')
-  .get(protect, getUsersList)
-  .post(registerUser);
+router.post('/', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', protect, logOutUser);
 router.route('/refresh_token').post(refreshToken);
