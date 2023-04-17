4710cd25cff23977de12aed114bf031965d9aaf4
shovoncse <shovondas26@gmail.com>
2023-04-18 00:58:24 +0600

    post edit related update

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,14 +5,15 @@ const {
   createPost,
   deletePost,
   getPostById,
+  updatePost
 } = require('../controllers/postController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(protect, getAllPosts).post(protect, createPost);
+router.route('/').post(protect, createPost);
+router.route('/').get(protect, getAllPosts);
 
-router
-  .route('/:id')
-  .get(protect, getPostById)
-  .delete(protect, deletePost);
+router.route('/:id').delete(protect, deletePost);
+router.route('/:id').get(protect, getPostById);
+router.route('/:id').put(protect, updatePost);
 
 module.exports = router;
