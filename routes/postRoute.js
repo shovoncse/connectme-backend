d0ed3079ca226addb7c12d7888e952934732c785
shovoncse <shovondas26@gmail.com>
2023-04-25 23:11:32 +0600

    comment and like features

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,6 +5,10 @@ const {
   createPost,
   deletePost,
   getPostById,
+  likePost,
+  likeComment,
+  createComment,
+  deleteComment,
   updatePost
 } = require('../controllers/postController');
 const { protect } = require('../middleware/authMiddleware');
@@ -15,5 +19,12 @@ router.route('/').get(protect, getAllPosts);
 router.route('/:id').delete(protect, deletePost);
 router.route('/:id').get(protect, getPostById);
 router.route('/:id').put(protect, updatePost);
+router.route('/:id').post(protect, createComment);
+
+router.route('/:id/like').get(protect, likePost);
+router
+  .route('/:id/:comId')
+  .get(protect, likeComment)
+  .delete(protect, deleteComment);
 
 module.exports = router;
